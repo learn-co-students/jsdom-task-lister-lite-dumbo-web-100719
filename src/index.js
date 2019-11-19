a4a598fb809ef3b6f61f956d82439307b26c2202
Gene Grady <grady.gene.thomas@gmail.com>
2019-11-19 12:29:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querlearnySelector("#create-task-form")
+  taskForm.addEventListener("submit", newTask)
 });
+
+let 
+
+const newTask = event => {
+  event.preventDefault();
+  const findNewTask = document.getElementById("new-task-description");
+  const createNewTask = document.createElement("li");
+  createNewTask.innerText = findNewTask.value;
+  appendNewTask(createNewTask);
+  event.target.reset();
+
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").append(task)
+}
+
