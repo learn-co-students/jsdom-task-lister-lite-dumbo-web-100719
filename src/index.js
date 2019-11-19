0e1121044f2a796e7a71e72b21c14e0fefe34027
Vannida Lim <vannida.lim@gmail.com>
2019-11-19 17:29:06 -0500

    Done v.2

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  
-});
-​
-let submitButton = document.querySelector("input[type='submit']")
-​
-submitButton.addEventListener("click", function(event){
- 
-  event.preventDefault();
-  let inputText = document.querySelector("#new-task-description")
-  debugger
-​
-  // create new li and update with content
-  
-  let newTaskLi = document.createElement("li")
-  newTaskLi.innerText = inputText.value
-​
-  // create delete button on li tag and add the "x" content
-  let newTaskLiButton = document.createElement("button")
-  newTaskLiButton.id = "delete-button"
-  newTaskLiButton.innerText = "x"
-  newTaskLi.append(newTaskLiButton)
-​
-  // find place on the DOM i.e. ul and then appending new li including content
-  let taskListUl = document.querySelector("#tasks")
-  taskListUl.append(newTaskLi)
-​
-  //function to delete task
-  newTaskLiButton.addEventListener("click", function(li){
-    newTaskLi.remove()
-  })
-​
+let form = document.querySelector("#create-task-form")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let taskLi = document.createElement("li")
+    let taskContent = document.querySelector('#new-task-description')
+    let taskUl = document.querySelector('#tasks')
+    
+    taskLi.innerText = taskContent.value
+    taskUl.append(taskLi)
+
+    let taskLiDeleteButton = document.createElement("button")
+    taskLiDeleteButton.innerText = "X"
+    taskLi.append(taskLiDeleteButton)
+
+    taskLiDeleteButton.addEventListener("click", () => {
+        taskLi.remove()
+    })
 })
+
+
