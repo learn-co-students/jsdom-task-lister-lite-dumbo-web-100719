a93dcc7ed1f91a0753bed3681700df177c1e1972
Hope <hopegiometti@gmail.com>
2019-11-19 10:13:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
   let newTaskForm = document.querySelector("#create-task-form")
   newTaskForm.addEventListener("submit", addTask)
   let newTaskDescription = document.querySelector("#new-task-description")
+  let todoListUl = document.querySelector("ul")
+  let newTaskLi = document.createElement("li")
+
+  
+  
+
 
   function addTask(){
-    event.preventDefault()
-    let todoListUl = document.querySelector("ul")
-    let newTaskLi = document.createElement("li")
+    event.preventDefault()  
     newTaskLi.innerText = newTaskDescription.value
     todoListUl.append(newTaskLi)
+    let newButton = document.createElement("button")
+    newButton.innerHTML = `<button id="delete">X</button>`
+    newTaskLi.append(newButton)
+    let button = document.getElementById("delete")
+    button.addEventListener('click', deleteTask)
+    function deleteTask(){
+      newTaskLi.remove()
+    }
   }
+
+  
+
+
 });
