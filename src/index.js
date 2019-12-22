f7731a23e709d1659382f952dceb64f047ef648c
github username <github email address>
2019-12-22 00:25:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 let task = document.getElementById("new-task-description");
 let taskList = document.getElementById("tasks");
-
-
 let taskForm = document.getElementById('create-task-form')
+
 taskForm.addEventListener("submit", t => {
   t.preventDefault();
   let newTask = task.value
   takeTaskAndMakeItAnLi(newTask)
+  t.target.reset()
 })
 
 function takeTaskAndMakeItAnLi(task){
@@ -15,6 +15,7 @@ function takeTaskAndMakeItAnLi(task){
   taskList.append(newTaskLi)
 
   let button = document.createElement("button")
+  button.innerText = "x"
   newTaskLi.append(button)
   button.addEventListener("click", () => {
     newTaskLi.remove()
