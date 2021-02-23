fb76729b8ed19bb5003f4222dc2c9bd1841e2028
javonesm89 <javosmith12@gmail.com>
2021-02-23 16:46:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  let taskBoard = document.getElementById('tasks')
+  
+
+
+  form.addEventListener('submit',function(e){
+    let newTask = document.getElementById('new-task-description').value
+    let taskLi = document.createElement('li')
+    taskLi.innerText = newTask
+    
+    let liDeleteBtn = document.createElement('button')
+    liDeleteBtn.innerText = "DELETE"
+    liDeleteBtn.addEventListener('click',function(){
+        this.parentNode.remove()
+    })
+    
+    let liEditBtn = document.createElement('button')
+    liEditBtn.innerText = "EDIT"
+    liEditBtn.addEventListener('click',function(){ 
+      
+    })
+    
+    
+    taskLi.append(liDeleteBtn)
+    taskLi.append(liEditBtn)
+    taskBoard.append(taskLi)
+    
+    e.preventDefault();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
